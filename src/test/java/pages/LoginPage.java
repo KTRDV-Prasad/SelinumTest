6a815fea08674e4c44f6b95df9fc712f421246d1
KOCHERLA TEJA RAJENDRA DURGA VARA PRASAD <133561397+KTRDV-Prasad@users.noreply.github.com>
2025-05-22 00:31:30 +0530

    Update LoginPage.java
    Updated Code Manually Updated Code Dumped here

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,26 +1,25 @@
 package pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 
 public class LoginPage {
 
-    private WebDriver driver; // Declare driver
-    private WebDriverWait wait; // Declare wait
+    WebDriver driver;
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));  // 10-second timeout
         PageFactory.initElements(driver, this);
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+            .until(ExpectedConditions.visibilityOf(txtUserName));
     }
-
-    // Correct locators based on current OrangeHRM login page
 
     @FindBy(how = How.NAME, using = "username")
     public WebElement txtUserName;
@@ -28,15 +27,18 @@ public class LoginPage {
     @FindBy(how = How.NAME, using = "password")
     public WebElement txtPassword;
 
-    @FindBy(how = How.CSS, using = "button[type='submit']")
+    @FindBy(how = How.CLASS_NAME, using = "orangehrm-login-button")
     public WebElement btnLogin;
 
     public void Login(String userName, String password) {
+        txtUserName.clear();
         txtUserName.sendKeys(userName);
+
+        txtPassword.clear();
         txtPassword.sendKeys(password);
     }
 
     public void ClickLogin() {
-        btnLogin.click();
+        btnLogin.click(); // Changed from .submit() to .click()
     }
 }
